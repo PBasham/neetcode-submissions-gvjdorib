055cb6e20d0438dbde7d58df62b05af9af1b1e59
Patrick Tyler Basham <90589626+PBasham@users.noreply.github.com>
2026-04-01 11:31:08 -0400

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        Stack<int> stack = new();
+
+        foreach(var token in tokens)
+        {
+            if(int.TryParse(token, out int num)) {stack.Push(num);}
+            else
+            {
+                switch (token) {
+                    case "+":
+                        stack.Push(stack.Pop() + stack.Pop());
+                        break;
+                    case "-":
+                        stack.Push(-stack.Pop() + stack.Pop());
+                        break;
+                    case "/":
+                        var first = stack.Pop();
+                        var second = stack.Pop();
+                        stack.Push(second / first);
+                        break;
+                    case "*":
+                        stack.Push(stack.Pop() * stack.Pop());
+                        break;
+                }
+            }
+        }
+
+        return stack.Peek();
+    }
+}
