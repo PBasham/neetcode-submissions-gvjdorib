64e135f4ea62a5f97f5e79821cd18ee9a6082f06
Patrick Tyler Basham <90589626+PBasham@users.noreply.github.com>
2026-04-05 17:07:44 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) {return false;}
+
+        Dictionary<char, int> counts = new();
+
+        for(int i = 0; i < s.Length; i++) 
+        {
+            counts[s[i]] = counts.GetValueOrDefault(s[i]) + 1;
+            counts[t[i]] = counts.GetValueOrDefault(t[i]) - 1;
+        }
+
+        foreach(var value in counts.Values)
+        {
+            if(value != 0 ) {return false;}
+        }
+
+        return true;
+    }
+}
