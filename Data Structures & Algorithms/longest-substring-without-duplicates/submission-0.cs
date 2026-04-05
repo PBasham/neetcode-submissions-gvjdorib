2dd41bb7fc5ab9bbe8f54e295d17588e7eb17c48
Patrick Tyler Basham <90589626+PBasham@users.noreply.github.com>
2026-04-05 19:59:41 -0400

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> seen = new();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for(int right = 0; right < s.Length; right++)
+        {
+            while(seen.Contains(s[right]))
+            {
+                seen.Remove(s[left]);
+                left++;
+            }
+
+            seen.Add(s[right]);
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
