9779f03e464597a231f893c28f50980928a4c904
Patrick Tyler Basham <90589626+PBasham@users.noreply.github.com>
2026-04-05 23:09:14 -0400

    Add: is-palindrome - submission-31

---
+++ Data Structures & Algorithms/is-palindrome/submission-31.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        var newString = string.Concat(s.ToLower().Where(c => char.IsDigit(c) || char.IsLetter(c)));
+
+        int left = 0;
+        int right = newString.Length - 1;
+
+        while(left < right)
+        {
+            
+            if(newString[left++] != newString[right--]) {return false;}
+        }
+        
+        return true;
+    }
+
+}
