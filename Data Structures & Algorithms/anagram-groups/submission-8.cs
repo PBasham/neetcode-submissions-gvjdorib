3d70f88dac1ca8a32b03697dbf81b568ed233820
Patrick Tyler Basham <90589626+PBasham@users.noreply.github.com>
2026-04-08 13:54:14 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> groups = new();
+
+        foreach(string str in strs)
+        {
+            string sorted = string.Concat(str.OrderBy(c => c));
+
+            if(!groups.ContainsKey(sorted)) {groups[sorted] = new List<string>();}
+
+            groups[sorted].Add(str);
+        }
+
+        return groups.Values.ToList();
+    }
+}
