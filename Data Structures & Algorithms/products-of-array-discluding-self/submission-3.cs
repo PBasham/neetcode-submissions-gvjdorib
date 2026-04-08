85a566c7f848f0a543dc9f1731051970985d77df
Patrick Tyler Basham <90589626+PBasham@users.noreply.github.com>
2026-04-08 14:59:56 -0400

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        var products = new int[nums.Length];
+        products[0] = 1;
+
+        for(int i = 1; i < nums.Length; i++){products[i] = products[i - 1] * nums[i - 1]; }
+
+        int suffix = 1;
+
+        for(int i = nums.Length - 1; i >= 0; i--) 
+        {
+            products[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return products;
+    }
+}
